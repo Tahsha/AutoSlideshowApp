7c09dceb00f51adf50702184949fd8eaeaab4433
takakoi <lifedesign.chris@gmail.com>
2020-11-17 22:20:55 +0900

    Initial commit

---
+++ app/src/main/java/com/example/autoslideshowapp/MainActivity.kt
@@ -0,0 +1,160 @@
+package com.example.autoslideshowapp
+
+import android.Manifest
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.content.pm.PackageManager
+import android.os.Build
+import android.provider.MediaStore
+import android.content.ContentUris
+import android.database.Cursor
+import android.transition.Slide
+import kotlinx.android.synthetic.main.activity_main.*
+import android.os.Handler
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val PERMISSIONS_REQUEST_CODE = 100
+
+
+    var cursor: Cursor? = null
+    private var mTimer: Timer? = null
+    private var mTimerSec = 1
+
+    private var mHandler = Handler()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Android 6.0以降の場合
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // パーミッションの許可状態を確認する
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                // 許可されている
+                getContentsInfo()
+            } else {
+                // 許可されていないので許可ダイアログを表示する
+                requestPermissions(
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    PERMISSIONS_REQUEST_CODE
+                )
+            }
+            // Android 5系以下の場合
+        } else {
+            getContentsInfo()
+        }
+
+
+
+
+
+        back_button.setOnClickListener{
+            if(cursor!!.isFirst()) {
+                cursor!!.moveToLast()
+            }else {
+                cursor!!.moveToPrevious()
+
+                val fieldIndex = cursor!!.getColumnIndex(MediaStore.Images.Media._ID)
+                val id = cursor!!.getLong(fieldIndex)
+                val imageUri =
+                    ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+                imageView.setImageURI(imageUri)
+
+                
+        }
+        }
+
+        forward_button.setOnClickListener{
+
+            if(cursor!!.isLast()){
+                cursor!!.moveToFirst()
+
+            }else{
+               cursor!!.moveToNext()
+
+            }
+            val fieldIndex = cursor!!.getColumnIndex(MediaStore.Images.Media._ID)
+            val id = cursor!!.getLong(fieldIndex)
+            val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+            imageView.setImageURI(imageUri)
+        }
+
+        play_button.setOnClickListener{
+            if (mTimer == null){
+                if (mTimer == null) {
+                    mTimer = Timer()
+                    play_button.setText("停止")
+                    back_button.isEnabled = false
+                    forward_button.isEnabled = false
+                    // タイマーの始動
+                    mTimer!!.schedule(object : TimerTask() {
+                        override fun run() {
+                            val fieldIndex = cursor!!.getColumnIndex(MediaStore.Images.Media._ID)
+                            val id = cursor!!.getLong(fieldIndex)
+                            val imageUri = ContentUris.withAppendedId(
+                                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                                id
+                            )
+                            cursor!!.moveToNext()
+                            if (cursor!!.isLast()) {
+                                cursor!!.moveToFirst()
+                            }
+
+                            mHandler.post {
+                                imageView.setImageURI(imageUri)
+                            }
+                        }
+                    }, 0, 2000) // 最初に始動させるまで 100ミリ秒、ループの間隔を 100ミリ秒 に設定
+                }
+
+            }else if (mTimer != null){
+                mTimer!!.cancel()
+                mTimer = null
+                play_button.setText("再生")
+                back_button.isEnabled = true
+                forward_button.isEnabled = true
+            }
+        }
+
+
+
+        }
+
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            PERMISSIONS_REQUEST_CODE ->
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getContentsInfo()
+                }
+        }
+
+
+    }
+
+    private fun getContentsInfo() {
+        val resolver = contentResolver
+        cursor = resolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
+            null, // 項目(null = 全項目)
+            null, // フィルタ条件(null = フィルタなし)
+            null, // フィルタ用パラメータ
+            null // ソート (null ソートなし)
+        )
+        cursor!!.moveToFirst()
+        // indexからIDを取得し、そのIDから画像のURIを取得する
+        val fieldIndex = cursor!!.getColumnIndex(MediaStore.Images.Media._ID)
+        val id = cursor!!.getLong(fieldIndex)
+        val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+
+        imageView.setImageURI(imageUri)
+
+    }
+
+}
+
+
+
+
